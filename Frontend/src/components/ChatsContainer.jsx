adde065b95f9b48b141bc26f16b998e762cc22cd
dlaz <dlaz@gmail.com>
2025-10-08 18:00:59 +0330

    last commit

--- Frontend/src/components/ChatsContainer.jsx
+++ Frontend/src/components/ChatsContainer.jsx
@@ -6,7 +6,6 @@ import { ChatContext } from '../context/ChatContext'
 import ImageModal from './ImageModal'
 import toast from 'react-hot-toast'
 import EmojiPicker from 'emoji-picker-react';
-import axios from 'axios'
 import { MdOutlineEmojiEmotions } from "react-icons/md";
 const ChatContainer = () => {
 
@@ -18,9 +17,10 @@ const ChatContainer = () => {
     const [imageUrl, setImageUrl] = useState('')
     const scrollEnd = useRef(null)
     const [input, setInput] = useState('')
+    const [showEmojiPicker, setShowEmojiPicker] = useState(false)
 
-const [showEmojiPicker,setShowEmojiPicker] = useState(false)
-   
+    const inputRef = useRef(null)
+
 
     //handle sending a message
     const handleSendMessage = async (e) => {
@@ -47,6 +47,13 @@ const [showEmojiPicker,setShowEmojiPicker] = useState(false)
         }
         reader.readAsDataURL(file)
     }
+    const handleEmojiButton = e=>{
+e.preventDefault()
+e.stopPropagation()
+setShowEmojiPicker(prev=>!prev)
+setTimeout(() => inputRef.current?.focus(), 0);
+    
+}
 
 
     useEffect(() => {
@@ -63,10 +70,10 @@ const [showEmojiPicker,setShowEmojiPicker] = useState(false)
     }, [messages])
 
     return selectedUser ? (
-        <div className='h-full overflow-y-hidden max-sm:relative backdrop-blur-lg '>
+        <div className='h-[100vh] sm:h-full overflow-y-hidden max-sm:relative backdrop-blur-lg '>
 
             {/* header */}
-            <div className='flex items-center gap-3 py-3 mx-4 border-b border-stone-500 '>
+            <div className='sticky inset-0 top-0 flex items-center gap-3 py-3 mx-4 border-b border-stone-500 '>
                 <img src={selectedUser.profilePic || assets.avatar_icon} alt="" className='w-8 rounded-full' />
                 <p className='flex-1 text-lg text-white flex items-center gap-2'>
                     {selectedUser.fullName}
@@ -80,73 +87,75 @@ const [showEmojiPicker,setShowEmojiPicker] = useState(false)
                     className='md:hidden max-w-7' />
 
             </div>
-             
-                            <div className='absolute left-1/2 top-1/2 -translate-y-1/2 -translate-x-1/2'>
-                                 <EmojiPicker 
-                                 width={300}
-                                 height={400}
-                                 open={showEmojiPicker} 
-                                 onEmojiClick={({emoji})=>setInput(prev=>prev+emoji)}/>
-                            </div>
-                         
 
-                    {/* chat area */}
-                    <div className='flex flex-col h-[calc(100%-120px)] overflow-y-auto p-3 pb-6 '>
-                        
-                        {isShowModal && <ImageModal imageUrl={imageUrl} setIsShowModal={setIsShowModal} />}
-                        {messages.map((message, idx) => (
-                            <div key={idx} className={`flex items-end gap-2 justify-end ${message.senderId !== authUser._id && 'flex-row-reverse'}`}>
+            <div className='absolute left-1/2 top-1/2 -translate-y-1/2 -translate-x-1/2'>
+                <EmojiPicker
+                    width={300}
+                    height={400}
+                    open={showEmojiPicker}
+                    autoFocusSearch={false}
+                    onEmojiClick={({ emoji }) => setInput(prev => prev + emoji)} />
+            </div>
 
-                                {
-                                    message.image ?
 
-                                        <img
-                                            onClick={() => showImageHandler(message.image)}
-                                            src={message.image} alt="" className='max-w-[230px] border border-gray-700 rounded-lg overflow-hidden mb-8 ' />
-                                        : <p className={`p-2 max-w-[200px] md:text-sm font-light rounded-lg mb-8 break-all bg-violet-500/30 text-white ${message.senderId == authUser._id ? 'rounded-br-none' : ''}`}>{message.text}</p>
-                                }
+            {/* chat area */}
+            <div className='flex flex-col h-[calc(100%-120px)] overflow-y-auto p-3 pb-6 '>
 
-                                <div className='text-center text-xs'>
-                                    <img src={message.senderId == authUser._id ? authUser?.profilePic || assets.avatar_icon : selectedUser.profilePic || assets.avatar_icon} className='w-7 rounded-full' />
-                                    <p className='text-gray-200'>{formatMessageTime(message.createdAt)}</p>
-                                </div>
+                {isShowModal && <ImageModal imageUrl={imageUrl} setIsShowModal={setIsShowModal} />}
+                {messages.map((message, idx) => (
+                    <div key={idx} className={`flex items-end gap-2 justify-end ${message.senderId !== authUser._id && 'flex-row-reverse'}`}>
 
-                            </div>
-                        ))}
-                        <div ref={scrollEnd}></div>
-                    </div>
-                    {/* bottom area */}
-                    <div className='absolute bottom-0 left-0 right-0 flex items-center  p-3'>
-                        
-                        <div className='flex-1 flex items-center bg-gray-100/12 px-3 rounded-full'>
-                        
-                            <input
-                                onChange={e => setInput(e.target.value)}
-                                value={input}
-                                onKeyDown={e => e.key === "Enter" ? handleSendMessage(e) : null}
-                                type="text"
-                                placeholder='send a message...'
-                                className='flex-1 text-sm p-3 border-none rounded-lg outline-none text-white placeholder-gray-400' />
-                            <input
-                                onChange={handleSendImage}
-                                type="file" id='image' accept='image/png, image/jpeg' hidden />
-                            <label htmlFor="image">
-                                <img src={assets.gallery_icon} alt="" className='w-5 mr-2 cursor-pointer' />
-                            </label>
-                            
-                             <MdOutlineEmojiEmotions
-                             color='white' 
-                             fontSize={25}
-                             cursor='pointer'
-                             onClick={()=>setShowEmojiPicker(prev=>!prev)}/>
+                        {
+                            message.image ?
+
+                                <img
+                                    onClick={() => showImageHandler(message.image)}
+                                    src={message.image} alt="" className='max-w-[230px] border border-gray-700 rounded-lg overflow-hidden mb-8 ' />
+                                : <p className={`p-2 max-w-[200px] md:text-sm font-light rounded-lg mb-8 break-all bg-violet-500/30 text-white ${message.senderId == authUser._id ? 'rounded-br-none' : ''}`}>{message.text}</p>
+                        }
+
+                        <div className='text-center text-xs'>
+                            <img src={message.senderId == authUser._id ? authUser?.profilePic || assets.avatar_icon : selectedUser.profilePic || assets.avatar_icon} className='w-7 rounded-full' />
+                            <p className='text-gray-200'>{formatMessageTime(message.createdAt)}</p>
                         </div>
-                        <img
-                            onClick={handleSendMessage}
-                            src={assets.send_button} alt="" className='w-7 cursor-pointer' />
+
                     </div>
-                
-               
-         
+                ))}
+                <div ref={scrollEnd}></div>
+            </div>
+            {/* bottom area */}
+            <div className='sticky inset-0 bottom-0 flex items-center  p-3'>
+
+                <div className='flex-1 flex items-center bg-gray-100/12 px-3 rounded-full'>
+
+                    <input
+                        ref={inputRef}
+                        onChange={e => setInput(e.target.value)}
+                        value={input}
+                        onKeyDown={e => e.key === "Enter" ? handleSendMessage(e) : null}
+                        type="text"
+                        placeholder='send a message...'
+                        className='flex-1 text-sm p-3 border-none rounded-lg outline-none text-white placeholder-gray-400 ' />
+                    <input
+                        onChange={handleSendImage}
+                        type="file" id='image' accept='image/png, image/jpeg' hidden />
+                    <label htmlFor="image">
+                        <img src={assets.gallery_icon} alt="" className='w-5 mr-2 cursor-pointer' />
+                    </label>
+
+                    <MdOutlineEmojiEmotions
+                        color='white'
+                        fontSize={25}
+                        cursor='pointer'
+                        onClick={handleEmojiButton}/>
+                </div>
+                <img
+                    onClick={handleSendMessage}
+                    src={assets.send_button} alt="" className='w-7 cursor-pointer' />
+            </div>
+
+
+
         </div>
     ) : (
         <div className='flex flex-col items-center justify-center gap-2 text-gray-500 bg-white/10 max-md:hidden'>
